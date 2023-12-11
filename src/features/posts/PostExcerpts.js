f875a1a6b1256cf0620b18121d9c1d8a8ae78df7
kaaviyas1906 <kaaviyasubramanian123@gmail.com>
2023-12-11 12:33:27 +0530

    Refactored UX

--- src/features/posts/PostExcerpts.js
+++ src/features/posts/PostExcerpts.js
@@ -1,40 +1,10 @@
-// import React from "react";
-// import PostAuthor from "./PostAuthor";
-// import ReactionButton from "./ReactionButton";
-// import TimeAgo from "./TimeAgo";
-// import {Link} from 'react-router-dom';
-
-// let PostExcerpts = ({ post }) => {
-//   return (
-//     <article>
-//       <h3>{post.title}</h3>
-
-//       <p className="excerpt">{post.body.substring(0,75)}...</p>
-
-//       <div className="postCredit">
-//         <Link to={`post/${post.id}`} >View Post</Link>
-//         <PostAuthor userId={post.userId} />
-//         <TimeAgo timestamp={post.date} />
-//         <ReactionButton post={post} />
-//       </div>
-      
-//     </article>
-//   );
-// };
-
-// //To render only the post that had updated 
-// PostExcerpts = React.memo(PostExcerpts);
-
-// export default PostExcerpts;
-
-
-//Using Normalization to reduce re-rendering
+import React from "react";
 import PostAuthor from "./PostAuthor";
 import ReactionButton from "./ReactionButton";
 import TimeAgo from "./TimeAgo";
 import {Link} from 'react-router-dom';
 
-const PostExcerpts = ({ post }) => {
+let PostExcerpts = ({ post }) => {
   return (
     <article>
       <h3>{post.title}</h3>
@@ -52,5 +22,8 @@ const PostExcerpts = ({ post }) => {
   );
 };
 
+//To render only the post that had updated 
+PostExcerpts = React.memo(PostExcerpts);
 
 export default PostExcerpts;
+
